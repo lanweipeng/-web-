15c5491da75666df33134d8f4ed93d5b38cb6e10
lwp <1462191066@qq.com>
2019-07-13 10:29:59 +0800

    初始化

---
+++ vue/hm/frontEnd/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>这是 App 组件</h1>
+  </div>
+</template>
+import  {Header} from 'mint-ui'
+
+<script>
+export default{
+// nwe Vue({
+
+// })
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue/hm/frontEnd/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue/dist/vue.esm'
+import App from './App.vue'
+new Vue({
+  el:'#app',
+  components:{App},
+  template:'<App/>'
+})
